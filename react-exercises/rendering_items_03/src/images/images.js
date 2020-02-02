a3b50a85b95db2d8dd5dbf85612bfdaefcfa1c16
Matina Patsos <matinaspatsos@gmail.com>
2020-02-02 12:58:50 -0500

    Removing future exercises

--- react-exercises/rendering_items_03/src/images/images.js
+++
@@ -1,8 +0,0 @@
-export const images = [
-  "https://i.picsum.photos/id/3/200/300.jpg",
-  "https://i.picsum.photos/id/4/200/300.jpg",
-  "https://i.picsum.photos/id/5/200/300.jpg",
-  "https://i.picsum.photos/id/6/200/300.jpg",
-  "https://i.picsum.photos/id/7/200/300.jpg",
-  "https://i.picsum.photos/id/20/200/300.jpg"
-];
--- react-exercises/state_and_forms/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Address from "./components/Address/Address";
-
-function App() {
-  return <Address />;
-}
-
-export default App;
