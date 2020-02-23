a1a824885ce8ead858e1a9c120c583b000cd69bb
Matina Patsos <matinaspatsos@gmail.com>
2020-02-23 18:54:09 -0500

    Lift state up exercise

---
+++ exercises/07-lifting-state-up/src/components/TemperatureScale/TemperatureScale.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function TemperatureScale(props) {
+  // Complete me
+  return <></>;
+}
+
+TemperatureScale.propTypes = {
+  setScale: PropTypes.func.isRequired
+};
+
+export default TemperatureScale;
