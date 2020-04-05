542013288f6dd081ba70dab177f241683a5a83cd
Louis345 <jt125845@gmail.com>
2020-04-05 15:54:27 -0400

    adding AJAX challenge

---
+++ exercises/13-class-ajax/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+/** Convert me into a class component  */
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ron Swanson</h1>
+    </div>
+  );
+}
+
+export default App;
